6152e082f8a9a39e93ff8bf7be117eda4699e0a3
Leonardo Palluzzi <lp.palluzzi@gmail.com>
2025-06-24 12:00:31 +0200

    fix unused imports

--- src/main/java/org/pizzeria/java/spring_la_mia_pizzeria_crud/controllers/PizzaController.java
+++ src/main/java/org/pizzeria/java/spring_la_mia_pizzeria_crud/controllers/PizzaController.java
@@ -8,15 +8,10 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-
-import jakarta.validation.Valid;
 
 @Controller
 @RequestMapping("/menu")
--- src/main/java/org/pizzeria/java/spring_la_mia_pizzeria_crud/model/Pizza.java
+++ src/main/java/org/pizzeria/java/spring_la_mia_pizzeria_crud/model/Pizza.java
@@ -8,7 +8,6 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Lob;
 import jakarta.persistence.Table;
-import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
